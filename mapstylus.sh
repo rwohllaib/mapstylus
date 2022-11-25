5a963a5662fe424e626db979b13826f3652992c4
baldr <53195105+rwohllaib@users.noreply.github.com>
2022-11-25 11:38:56 -0700

    Create mapstylus.sh

---
+++ mapstylus.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 'Did you touch the stylus to the screen yet?'
+while true; do
+    read -p "(y)es/(n)o :" yn
+    case $yn in
+        [yY] ) echo user selected yes;
+            break;;
+        [nN] ) echo exiting;   
+            exit;; 
+        * ) echo try again;;
+    esac
+
+done
+
+echo proceeding...
+
+# pull stylus id
+stylusidraw=$(xinput | grep "Stylus Pen" | cut -d$'\t' -f 2)
+echo 'stylusidraw is '$stylusidraw
+stylusid=$(echo $stylusidraw | cut -d '=' -f 2)
+echo 'stylus id is' $stylusid
+
+#monitor value, check via xrandr --query, mine is eDP yours might not be
+
+xinput map-to-output $stylusid eDP
+
+echo 'complete'
